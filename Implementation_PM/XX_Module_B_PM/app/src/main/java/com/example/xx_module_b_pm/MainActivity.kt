dcedfc26aac6c7263da142c85830ce73424a98b2
decla <declanisaackuok@gmail.com>
2024-09-29 16:20:59 +0800

    f

---
+++ Implementation_PM/XX_Module_B_PM/app/src/main/java/com/example/xx_module_b_pm/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.xx_module_b_pm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.xx_module_b_pm.ui.theme.XX_Module_B_PMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            XX_Module_B_PMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    XX_Module_B_PMTheme {
+        Greeting("Android")
+    }
+}
